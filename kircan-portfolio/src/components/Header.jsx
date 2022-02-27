45d0c27f5f231e611d2051f3297d106070eef1ce
PacDraft <Kirsten@PacificDraftWorks.com>
2022-02-26 19:51:06 -0800

    finished working on header code

--- kircan-portfolio/src/components/Header.jsx
+++ kircan-portfolio/src/components/Header.jsx
@@ -3,7 +3,51 @@
 import React, { useState } from 'react';
 import Navbar from "./Navbar";
 import About from "./About";
-import Contact from "./Contact";
 import Portfolio from "./Portfolio";
+import Contact from "./Contact";
 import Resume from "./Resume";
 
+function createHeader() {
+    const [currentPage, handlePageChange] = useState("About");
+
+    //use the renderPage method to utilize a switch statement to render the correct page
+
+    const renderPage = () => {
+        switch (currentPage) {
+            case "About":
+                return <About />;
+            case "Portfolio": 
+                return <Portfolio />;
+            case "Contact": 
+                return <Contact />;
+            case "Resume": 
+                return <Resume />;
+
+            default:
+                return <About />;
+        }
+    };
+
+    return (
+        <div>
+            <nav className = "navbar">
+                <div className = "navbar-brand">
+                    <a className = "navbar-item"
+                    rel = "noreferrer"
+                    target = "_blank"
+                    href = "https://github.com/KGCan">
+                        <span className = "content is-large">Kirsten Canady</span>
+                    </a>
+                </div>
+            </nav>
+            <Navbar
+            currentPage = {currentPage}
+            handlePageChange = {handlePageChange}/>
+            <main>
+                <div>{renderPage(currentPage)}</div>
+            </main>
+        </div>
+    );
+}
+
+export default createHeader;
