e95e872724080cd317030a866b1ed117681ec7f7
PacDraft <Kirsten@PacificDraftWorks.com>
2022-02-27 13:33:49 -0800

    working on fixing some styling

--- kircan-portfolio/src/components/Project.jsx
+++ kircan-portfolio/src/components/Project.jsx
@@ -3,12 +3,12 @@ import React from "react";
 function Project(props) {
   return (
     <div>
-      <div className="columns is-desktop is-justify-content-center is-flex-wrap-wrap is-flex-direction-row">
+      <div className="columns is-justify-content-center is-flex-wrap-wrap is-flex-direction-row">
         {props.projects.map((project) => (
           <div className="column is-half">
             <div className="card">
               <div className="card-image">
-                <figure className="image is-3">
+                <figure className="image">
                   <a href={project.live} target="_blank" rel="noreferrer">
                     <img src={process.env.PUBLIC_URL + project.image} alt="Placeholder" />
                   </a>
