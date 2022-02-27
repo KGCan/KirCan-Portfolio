2d9ff71aa0c84b49814f7c5c5e8c72745299514d
PacDraft <Kirsten@PacificDraftWorks.com>
2022-02-26 19:12:19 -0800

    added component for resume, added links in header to other components, added resume pdf

---
+++ kircan-portfolio/src/components/Header.jsx
@@ -0,0 +1,9 @@
+// Links to various components
+
+import React, { useState } from 'react';
+import Navbar from "./Navbar";
+import About from "./About";
+import Contact from "./Contact";
+import Portfolio from "./Portfolio";
+import Resume from "./Resume";
+
