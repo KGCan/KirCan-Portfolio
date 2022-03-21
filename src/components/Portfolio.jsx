60d5917657720d2c34e08606ede7a451c2495082
PacDraft <Kirsten@PacificDraftWorks.com>
2022-03-20 17:35:01 -0700

    added project 3 to porfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -56,6 +56,15 @@ const projects = [
         repoURL: "https://github.com/KGCan/Basic-Budget-Tracker.git",
         deployedURL: "https://basic-budget-tracker-app.herokuapp.com/",
     },
+    {
+        id: 6, 
+        projectTitle: "Nosh-Up",
+        languagesUsed: "MongoDb, Express, React, Node, Bootstrap, Heroku, MVC Structuring",
+        image: "/images/noshup",
+        projectDescription: "A site to search events to meet-up with new people who like the same foods as you. The user will have the ability to sign up and create a user profile that will be visible to other users. A logged in user can create an event, view the events they are hosting as well as any other events they have joined. Users can join existing events from other users, view all events and click on a specific event for more details. Once a specific event has been selected the user can click on the host's profile to view their information and/or click to join the event.",
+        repoURL: "https://github.com/limichael97/Nosh-Up.git",
+        deployedURL: "http://nosh-up.herokuapp.com/",
+    },
 ];
 
 function createPortfolio() {
